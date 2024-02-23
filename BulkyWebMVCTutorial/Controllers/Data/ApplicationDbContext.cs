cf79686aaeb314d7e330df062411fd0b4cdbe12b
Christopher Bryan <christopher_bryan@rocketmail.com>
2024-02-23 08:46:19 -0800

    initial commit

---
+++ BulkyWebMVCTutorial/Controllers/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebMVCTutorial.Controllers.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ BulkyWebMVCTutorial/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWebMVCTutorial.Models
+{
+    public class Category
+    {
+        [Key] // by default, if prop is Id or [Class Name]Id, then .NET auto assumes it is a primary key
+        public int Id { get; set; }
+        [Required] // Data annotations
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
