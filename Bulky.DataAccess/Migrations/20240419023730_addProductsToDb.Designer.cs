eeb72b73c277a454bc4ac6e44dd6c71eacd23f20
Christopher Bryan <christopher_bryan@rocketmail.com>
2024-04-18 19:47:19 -0700

    products migration

--- Bulky.DataAccess/Migrations/20240419023730_addProductsToDb.Designer.cs
+++ Bulky.DataAccess/Migrations/20240419023730_addProductsToDb.Designer.cs
@@ -2,6 +2,7 @@
 using Bulky.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Bulky.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240419023730_addProductsToDb")]
+    partial class addProductsToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Bulky.Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models
+{
+    public class Product
+    {
+        [Key] // by default, if prop is Id or [Class Name]Id (CategoryId), then .NET auto assumes it is a primary key
+        public int Id { get; set; }
+        [Required] // Data annotations
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required] // Data annotations
+        public string ISBN { get; set; }
+        [Required] // Data annotations
+        public string Author { get; set; }
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1, 1000)]
+        public double ListPrice { get; set; }
+        [Required]
+        [Display(Name = "Price for 1-50")]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+        [Required]
+        [Display(Name = "Price for 50+")]
+        [Range(1, 1000)]
+        public double Price50 { get; set; }
+        [Required]
+        [Display(Name = "Price for 100+")]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
+    }
+}
