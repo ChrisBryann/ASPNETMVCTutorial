5b56eb5ba261211c11abb11eb33428fa8a347b59
Christopher Bryan <christopher_bryan@rocketmail.com>
2024-04-24 22:46:50 -0700

    details page and identity

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Bulky.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bulky.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext // anything we have to do with database, we do it here
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser> // anything we have to do with database, we do it here
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -14,7 +16,7 @@ namespace Bulky.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(modelBuilder); // keys of identity table are mapped in OnModelCreating
             modelBuilder.Entity<Category>().HasData(
                 new Category {Id=1, Name="Action", DisplayOrder=1},
                 new Category {Id=2, Name="SciFi", DisplayOrder=2},
--- BulkyWebMVCTutorial/Program.cs
+++ BulkyWebMVCTutorial/Program.cs
@@ -2,12 +2,16 @@ using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>(); // AddEntitFrameworkStores adds the identity tables (users, roles, etc), and RequiredConfirmAccount is to tell if the account should be confirmed or not
 // inject the categoryRepository service
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 // adding DI types - transient, scoped, singleton
@@ -31,9 +35,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
